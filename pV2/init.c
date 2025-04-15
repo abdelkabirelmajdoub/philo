e8f14de7a6b67599d509f280f1bcbb587aeb3a67
SMART <56655744+abdelkabirelmajdoub@users.noreply.github.com>
2025-04-15 14:53:45 +0100

    Add files via upload

---
+++ pV2/init.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-majd <ael-majd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 18:18:50 by ael-majd          #+#    #+#             */
+/*   Updated: 2025/04/15 13:45:17 by ael-majd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	data_init(char **av, t_data *data)
+{
+	int	i;
+
+	data->nbr_philos = ft_atoi(av[1]);
+	data->t_die = ft_atoi(av[2]);
+	data->t_eat = ft_atoi(av[3]);
+	data->t_sleep = ft_atoi(av[4]);
+	if (av[5])
+		data->meals_req = ft_atoi(av[5]);
+	else
+		data->meals_req = -1;
+	pthread_mutex_init(&data->death_check, NULL);
+	pthread_mutex_init(&data->print, NULL);
+	pthread_mutex_init(&data->meal_check, NULL);
+	data->start_time = get_time();
+	i = 0;
+	while(i < data->nbr_philos)
+	{	
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+			return (1);
+		i++;
+	}
+	return (0);	
+}
+
+int	philo_init(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	while(++i < data->nbr_philos)
+	{
+		data->philo[i].id = i + 1;
+		data->philo[i].meals_eaten = 0;
+		data->philo[i].last_meal = data->start_time;
+		data->philo[i].data = data;
+		data->philo[i].l_fork = &data->forks[i];
+		data->philo[i].r_fork = &data->forks[(i + 1) % data->nbr_philos];
+	}
+	return (0);
+}
+
+int init(t_data *data, char **av)
+{
+	if (data_init(av, data))
+		return (1);
+	if (philo_init(data))
+		return (1);
+	return (0);
+}
---
+++ pV2/utils.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-majd <ael-majd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 16:19:48 by ael-majd          #+#    #+#             */
+/*   Updated: 2025/04/15 13:51:18 by ael-majd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	check_input(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac < 5 || ac > 6)
+		return (write(2, "Wrong args count\n",17), 1);
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (!(av[i][j] >= '0' && av[i][j] <= '9'))
+				return (printf("Bad args: Invalid input\n"), 1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_atoi(char *s)
+{
+	int		i;
+	long	res;
+
+	i = 0;
+	res = 0;
+	while (s[i] && s[i] == ' ')
+		i++;
+	while (s[i] && s[i] >= '0' && s[i] <= '9')
+	{
+		res = res * 10 + (s[i] - '0');
+		if (res > 2147483647)
+			return (-1);
+		i++;
+	}
+	return (res);
+}
+size_t	get_time(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		write(2, "gettimeofday() error\n", 22);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+int	ft_usleep(size_t milliseconds)
+{
+	size_t	start;
+
+	start = get_time();
+	while ((get_time() - start) < milliseconds)
+		usleep(100);
+	return (0);
+}
